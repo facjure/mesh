25680984f63c0db33db4820e64357101d979f3e9
Priyatam Mudivarti <priyatam@gmail.com>
2015-04-22 19:45:40 -0700

    added breakpoint macro
    credit: https://github.com/noprompt/noprompt.github.io/blob/master/clojure/blog/src/blog/util.clj

--- src/clj/mesh/def.clj
+++ src/clj/mesh/def.clj
@@ -9,3 +9,8 @@
   "Convenience macro equivalent to `(def name (list styles*))`."
   [name & styles]
   `(def ~name (list ~@styles)))
+
+(defmacro defbreakpoint [name media-params]
+  `(defn ~name [& rules#]
+     (at-media ~media-params
+       [:& rules#])))
