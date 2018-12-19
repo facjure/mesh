272d902e558abfd91dbed69085339d1b14271748
Priyatam Mudivarti <priyatam@facjure.com>
2018-12-18 17:46:27 -0800

    update deps, dev workflows

---
+++ dev/repl.cljs
@@ -0,0 +1,18 @@
+(ns dev.repl
+  (:require-macros
+   [figwheel.client.utils :refer [enable-dev-blocks!]])
+  (:require [examples.hello.page :as hello]
+            [examples.typography.page :as typo]
+            [examples.grids.page :as grids]
+            [figwheel.client :as fw]))
+
+(enable-console-print!)
+(enable-dev-blocks!)
+
+(print "Starting Figwheel Cljs Repl ... ")
+
+(fw/start
+ {:websocket-url "ws://localhost:3449/figwheel-ws"
+  :on-jsload (fn []
+               (print "Figwheel ... js loaded "))
+  :load-warninged-code true})
